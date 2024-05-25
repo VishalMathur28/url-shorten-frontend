44ee557089b8fc274d1f9696062c97c78db434cb
Vishal Mathur <vishalmathur.xspf@gmail.com>
2024-05-25 16:15:22 +0530

    first init url shorten

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'myApp';
+  originalUrl: string = '';
+  shortenedUrl: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  shortenUrl() {
+    this.http
+      .post<any>('http://localhost:3000/shorten', { url: this.originalUrl })
+      .subscribe({
+        next: (res: any) => {
+          this.shortenedUrl = res.shortUrl;
+        },
+        error: (err: any) => {
+          console.error('Error occurred:', err);
+        },
+      });
+  }
 }
